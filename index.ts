8719ec4f64ddb612393b1bca86c542f60e47974a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 21:01:48 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { from } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/creation/from
+// Example 4: Observable from string
+
 //emit string as a sequence
 const source = from('Hello World');
 //output: 'H','e','l','l','o',' ','W','o','r','l','d'
-const subscribe = source.subscribe(val => console.log(val));
+const subscribe = source.subscribe((val) => console.log(val));
