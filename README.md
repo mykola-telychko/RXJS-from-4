c43c00ac2b3289bb369c90c9615e220ed2ce7f78
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 21:00:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-from-4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-o28azx)
---
+++ index.ts
@@ -0,0 +1,6 @@
+import { from } from 'rxjs';
+
+//emit string as a sequence
+const source = from('Hello World');
+//output: 'H','e','l','l','o',' ','W','o','r','l','d'
+const subscribe = source.subscribe(val => console.log(val));
